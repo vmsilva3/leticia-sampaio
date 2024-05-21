5151c343a2b47dd6a3246263c110661276b5b6f7
Vitor Silva <vitorsilva.m33@gmail.com>
2024-05-20 22:02:39 -0300

    Scroll animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+function scrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+scrollSuave();
